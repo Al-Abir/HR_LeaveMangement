e9ecbadf9c7a21a6c6cc07f0f58ecbef1e370b4a
Al-Abir <sheikhabir196@gmail.com>
2026-02-16 14:49:31 +0600

    DbContext

---
+++ HR.LeaveMangement.Persistence/LeaveMangementDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveMangement.Domain;
+using HR.LeaveMangement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+namespace HR.LeaveMangement.Persistence
+{
+    public class LeaveMangementDbContext:DbContext
+    {
+
+        public LeaveMangementDbContext(DbContextOptions<LeaveMangementDbContext> options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveMangementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>()){
+
+                entry.Entity.LastModifiedDate = DateTime.Now;
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        public DbSet<LeaveAllocation> LeaveAllocation { get; set; }
+    }
+}
