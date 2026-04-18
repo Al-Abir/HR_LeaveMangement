9a3c748f9da6a91ac4f1b1c5f6d451ceca8dd2e4
Al-Abir <sheikhabir196@gmail.com>
2026-04-18 16:06:17 +0600

    data

--- src/Infrastructure/HR.LeaveMangement.Persistence/Repositories/LeaveTypeRepository.cs
+++ src/Infrastructure/HR.LeaveMangement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HR.LeaveMangement.Application.Persistence.Contracts;
+using HR.LeaveMangement.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveMangement.Persistence.Repositories
 {
-    public class LeaveTypeRepository
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
     {
+
+        private readonly LeaveMangementDbContext _dbcontext;
+
+        public LeaveTypeRepository(LeaveMangementDbContext dbcontext) :base(dbcontext) 
+        {
+            _dbcontext = dbcontext;
+        }
     }
 }
