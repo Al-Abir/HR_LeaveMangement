ab1a4a9c0b4dbf98ba60e7a348a0e315767568c8
Al-Abir <sheikhabir196@gmail.com>
2026-04-24 16:13:27 +0600

    Persistence

--- src/Infrastructure/HR.LeaveMangement.Persistence/Repositories/LeaveAllocationRepository.cs
+++ src/Infrastructure/HR.LeaveMangement.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -1,5 +1,7 @@
-﻿using HR.LeaveMangement.Application.Persistence.Contracts;
+﻿using HR.LeaveMangement.Application.Exceptions;
+using HR.LeaveMangement.Application.Persistence.Contracts;
 using HR.LeaveMangement.Domain;
+using HR.LeaveMangement.Persistence;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,26 +10,32 @@ namespace HR.LeaveMangement.Persistence.Repositories
 {
     public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
     {
-        private readonly LeaveManagementDbContext _dbContext;
+        private readonly LeaveMangementDbContext _dbContext;
 
         // ১. কনস্ট্রাক্টর (এটি ছাড়া GenericRepository কাজ করবে না)
-        public LeaveAllocationRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        public LeaveAllocationRepository(LeaveMangementDbContext dbContext) : base(dbContext)
         {
             _dbContext = dbContext;
         }
 
         // ২. নির্দিষ্ট একটি আইডি দিয়ে ডিটেইলস আনা
-        public async Task<LeaveAllocation?> GetLeaveAllocationWithDetails(int id)
+        public async Task<LeaveAllocation> GetLeaveAllocationWithDetails(int id)
         {
-            return await _dbContext.LeaveAllocations
-                .Include(q => q.LeaveType) // LeaveType এর তথ্যসহ লোড হবে
-                .FirstOrDefaultAsync(q => q.Id == id);
+            var result = await _dbContext.LeaveAllocation
+    .Include(q => q.LeaveType)
+    .FirstOrDefaultAsync(q => q.Id == id);
+
+            if (result == null)
+            {
+                throw new NotFoundException(nameof(LeaveAllocation), id);
+            }
+            return result;
         }
 
         // ৩. সব অ্যালোকেশনের লিস্ট ডিটেইলসসহ আনা
         public async Task<List<LeaveAllocation>> GetLeaveAllocationWithDetails()
         {
-            return await _dbContext.LeaveAllocations
+            return await _dbContext.LeaveAllocation
                 .Include(q => q.LeaveType)
                 .ToListAsync();
         }
